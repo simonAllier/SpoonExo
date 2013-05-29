9145f0099ad8b485f17ba3f8a6da5c97a22ef807
Simon Allier <simon.allier@gmail.com>
2013-05-29 18:25:54 +0200

    first commit

---
+++ src/main/java/fr/inria/Main.java
@@ -0,0 +1,46 @@
+package fr.inria;
+
+import spoon.processing.ProcessingManager;
+import spoon.reflect.Factory;
+import spoon.support.DefaultCoreFactory;
+import spoon.support.QueueProcessingManager;
+import spoon.support.StandardEnvironment;
+import spoon.support.builder.SpoonBuildingManager;
+
+import java.io.File;
+
+/**
+ * User: Simon
+ * Date: 5/29/13
+ * Time: 5:44 PM
+ */
+public class Main {
+
+    public static void main(String [ ] args) throws Exception {
+        Main main = new Main();
+        main.runSpoon(args[0]);
+    }
+
+
+    protected void runSpoon(String sourceDirectory) {
+        StandardEnvironment env = new StandardEnvironment();
+        env.setComplianceLevel(6);
+        env.setVerbose(true);
+        env.setDebug(true);
+
+        DefaultCoreFactory f = new DefaultCoreFactory();
+        Factory factory = new Factory(f, env);
+        SpoonBuildingManager builder = new SpoonBuildingManager(factory);
+        for(String dir : sourceDirectory.split(System.getProperty("path.separator")))
+            try {
+                builder.addInputSource(new File(dir));
+                builder.build();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        ProcessingManager pm = new QueueProcessingManager(factory);
+        StatementProcessor processor = new StatementProcessor();
+        pm.addProcessor(processor);
+        pm.process();
+    }
+}
---
+++ src/main/java/fr/inria/StatementProcessor.java
@@ -0,0 +1,21 @@
+package fr.inria;
+
+import spoon.processing.AbstractProcessor;
+import spoon.reflect.code.CtStatement;
+
+/**
+ * User: Simon
+ * Date: 5/29/13
+ * Time: 6:00 PM
+ */
+public class StatementProcessor extends AbstractProcessor<CtStatement> {
+    int i = 0;
+
+
+    public void process(CtStatement element) {
+        i++;
+        System.out.println("Statement "+ i);
+        System.out.println(element);
+        System.out.println("-----------------------------------");
+    }
+}
